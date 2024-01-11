635c5c6c5205e9f63824818d0758e8d28c7482e4
zleisure <993552798@qq.com>
2024-01-11 17:56:24 +0800

    Update web_gl.js

--- web_gl.js
+++ web_gl.js
@@ -17,7 +17,7 @@ function main() {
   }
 
   // Set clear color to black, fully opaque
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearColor(1.0, 0.0, 0.0, 1.0);
   // Clear the color buffer with specified clear color
   gl.clear(gl.COLOR_BUFFER_BIT);
 }
