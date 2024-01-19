9905c3267bf95ae5bebe12c212cf3307ad64fd97
zleisure <993552798@qq.com>
2024-01-19 10:41:29 +0800

    Update main.js

--- main.js
+++ main.js
@@ -5,22 +5,22 @@ main();
 // start here
 //
 function main() {
-  const canvas = document.querySelector("#gl_canvas");
-  // Initialize the GL context
-  const gl = canvas.getContext("webgl");
+  // const canvas = document.querySelector("#gl_canvas");
+  // // Initialize the GL context
+  // const gl = canvas.getContext("webgl");
 
-  // Only continue if WebGL is available and working
-  if (gl === null) {
-    alert(
-      "Unable to initialize WebGL. Your browser or machine may not support it.",
-    );
-    return;
-  }
+  // // Only continue if WebGL is available and working
+  // if (gl === null) {
+  //   alert(
+  //     "Unable to initialize WebGL. Your browser or machine may not support it.",
+  //   );
+  //   return;
+  // }
 
-  // Set clear color to black, fully opaque
-  gl.clearColor(1.0, 0.0, 0.0, 1.0);
-  // Clear the color buffer with specified clear color
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  // // Set clear color to black, fully opaque
+  // gl.clearColor(1.0, 0.0, 0.0, 1.0);
+  // // Clear the color buffer with specified clear color
+  // gl.clear(gl.COLOR_BUFFER_BIT);
 
   console.log("Hello GL");
   console.log(THREE.Scene);
